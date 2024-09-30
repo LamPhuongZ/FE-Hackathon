f41c3373e1c160d74cc963355e42619caacc3d15
LamphuongZ <lamphuong024@gmail.com>
2024-09-30 10:24:27 +0700

    feat: header-ui

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,14 @@
+import { Hero } from "./hero";
+import { Nav } from "./nav";
+import logoCompany from "../../assets/images/logo-company.png";
+
 export default function Header() {
-    return (
-        <header className="header">
-            <h1>This is the header</h1>
-        </header>
-    )
+  return (
+    <header className="header">
+      <img src={logoCompany} alt="logo-company" />
+      <h1>Việc Làm Dễ</h1>
+      <Nav />
+      <Hero />
+    </header>
+  );
 }
---
+++ src/components/header/nav/index.jsx
@@ -0,0 +1,11 @@
+export function Nav() {
+    return (
+        <nav className="nav">
+            <ul>
+                <li><a href="#">Tại Sao Chọn Chúng Tôi ?</a></li>
+                <li><a href="#">Quy Trình Của Chúng Tôi</a></li>
+                <li><a href="#">Về Chúng Tôi</a></li>
+            </ul>
+        </nav>
+    )
+}
