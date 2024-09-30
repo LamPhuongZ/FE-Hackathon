6b7b19cfd10d43b72e7826ff7ea85c0984400797
taytruong <taytruong05@gmail.com>
2024-09-30 22:20:42 +0700

    update 10:20 30/09

--- src/templates/HomeTemplate.jsx
+++ src/templates/HomeTemplate.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import HowItWorks from "../components/how-it-works/howItWorks";
@@ -6,14 +6,14 @@ import HowItWorks from "../components/how-it-works/howItWorks";
 export default function HomeTemplate() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <div
         className="content-layout pt-20 bg-white"
         style={{ minHeight: "80vh" }}
       >
         <HowItWorks></HowItWorks>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
